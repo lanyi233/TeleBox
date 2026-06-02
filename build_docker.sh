7a8e01b6ada2fed873a6089db13217a198aa00cd
lanyi233 <lanyi233@qq.com>
2026-06-02 23:17:54 +0800

    feat(Docker): Build Script Auto Install binfmt & Fix Type

--- build_docker.sh
+++ build_docker.sh
@@ -3,29 +3,30 @@ set -e
 
 get_platform() {
     case "${1:-$(uname -m)}" in
-        "x86_64" | "amd64")  echo "amd64" ;;
-        "aarch64" | "arm64") echo "arm64" ;;
-        *) echo "❌ 不支持的架构: $1" && exit 1 ;;
+        "x86_64" | "amd64")  echo "linux/amd64" ;;
+        "aarch64" | "arm64") echo "linux/arm64" ;;
+        *) echo "!! 不支持的架构: $1" >&2 && exit 1 ;;
     esac
 }
 
 TARGET_PLATFORM=$(get_platform "$1")
 BUILDER_PLATFORM=$(get_platform "$(uname -m)")
+ARCH_TAG=${TARGET_PLATFORM#linux/}
 
 echo ":: 本机架构: $BUILDER_PLATFORM"
 echo ":: 目标架构: $TARGET_PLATFORM"
 
 if [ "$TARGET_PLATFORM" = "$BUILDER_PLATFORM" ]; then
     echo " -> 原生构建"
-    docker build --platform "$TARGET_PLATFORM" -t "telebox:latest" .
+    echo " -> Build"
+    docker buildx build --platform "$TARGET_PLATFORM" -t "telebox:latest" --load .
 else
     echo " -> 交叉构建"
     export DOCKER_BUILDKIT=1
-    docker buildx build --platform "$TARGET_PLATFORM" -t "telebox:$TARGET_PLATFORM" --load . || {
-      echo "!! 缺少虚拟化支持, 运行命令以注册"
-      echo "docker run --privileged --rm tonistiigi/binfmt --install all"
-      echo "or"
-      echo "docker run --rm --privileged multiarch/qemu-user-static --reset -p yes"
-      exit 1
-    }
+    if ! find "/proc/sys/fs/binfmt_misc" -name "qemu-*" | grep -q qemu; then
+        echo " -> binfmt install"
+        docker run --privileged --rm tonistiigi/binfmt --install all
+    fi
+    echo " -> Build"
+    docker buildx build --platform "$TARGET_PLATFORM" -t "telebox:$ARCH_TAG" --load .
 fi
