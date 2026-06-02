205c61d3bdbfc49d8a041573ba2b3db45b978a39
lanyi233 <lanyi233@qq.com>
2026-06-02 21:23:58 +0800

    fix(Docker): Build Script build docker name unintended overwrite

--- build_docker.sh
+++ build_docker.sh
@@ -17,11 +17,11 @@ echo ":: 目标架构: $TARGET_PLATFORM"
 
 if [ "$TARGET_PLATFORM" = "$BUILDER_PLATFORM" ]; then
     echo " -> 原生构建"
-    docker build --platform "$TARGET_PLATFORM" -t telebox .
+    docker build --platform "$TARGET_PLATFORM" -t "telebox:latest" .
 else
     echo " -> 交叉构建"
     export DOCKER_BUILDKIT=1
-    docker buildx build --platform "$TARGET_PLATFORM" -t telebox --load . || {
+    docker buildx build --platform "$TARGET_PLATFORM" -t "telebox:$TARGET_PLATFORM" --load . || {
       echo "!! 缺少虚拟化支持, 运行命令以注册"
       echo "docker run --privileged --rm tonistiigi/binfmt --install all"
       echo "or"
